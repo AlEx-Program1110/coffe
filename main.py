a765a529c48003f12c881607ea3b0ef90e2cee08
AlEx-Program1110 <4028307@mail.ru>
2023-11-22 13:43:27 +0300

    start commit

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import sqlite3
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QTableWidget
-from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QTableWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QTableWidget
 
 
-class MyWidget(QMainWindow):
+# "ID", "название сорта", "степень обжарки", "молотый/в зернах", "описание вкуса", "цена", "объем упаковки"
+
+class Coffe_menu(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
+        self.information_coffe: QTableWidget
+        self.information_coffe.setColumnCount(7)
+        self.information_coffe.setHorizontalHeaderLabels(
+            ["ID", "название сорта", "степень обжарки", "молотый/в зернах", "описание вкуса", "цена", "объем упаковки"])
         self.con = sqlite3.connect("coffee.sqlite")
         self.cur = self.con.cursor()
 
@@ -16,11 +22,14 @@ class MyWidget(QMainWindow):
         print(self.result)
 
         self.con.close()
+        self.information_coffe.setRowCount(len(self.result))
+        for i in range(len(self.result)):
+            for j in range(len(self.result[i])):
+                self.information_coffe.setItem(i, j, QTableWidgetItem(str(self.result[i][j])))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MyWidget()
+    ex = Coffe_menu()
     ex.show()
     sys.exit(app.exec())
-

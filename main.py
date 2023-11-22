e46b165ff8fe327838cd1fe54afb1684df89cd7a
AlEx-Program1110 <4028307@mail.ru>
2023-11-22 17:04:56 +0300

    start new exercise commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sqlite3
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QTableWidgetItem
-from PyQt5.QtWidgets import QMainWindow, QTableWidget
+from PyQt5.QtWidgets import QApplication, QTableWidgetItem, QLineEdit
+from PyQt5.QtWidgets import QMainWindow, QTableWidget, QPushButton, QLabel
 
 
 # "ID", "название сорта", "степень обжарки", "молотый/в зернах", "описание вкуса", "цена", "объем упаковки"
@@ -25,6 +25,36 @@ class Coffe_menu(QMainWindow):
         for i in range(len(self.result)):
             for j in range(len(self.result[i])):
                 self.information_coffe.setItem(i, j, QTableWidgetItem(str(self.result[i][j])))
+
+        self.add: QPushButton
+        self.change: QPushButton
+
+        self.add.clicked.connect(self.push_add)
+        self.change.clicked.connect(self.push_change)
+
+    def push_add(self):
+        print('add')
+        self.ex = Dialog_add_change(self, 0)
+        self.ex.show()
+
+    def push_change(self):
+        print('change')
+        self.ex = Dialog_add_change(self, 1)
+        self.ex.show()
+
+
+class Dialog_add_change(QMainWindow):
+    def __init__(self, parent, move):
+        super().__init__(parent)
+        uic.loadUi('addEditCoffeeForm.ui', self)
+
+        self.data_new: QLineEdit
+        self.label: QLabel
+
+        if move:
+            self.label.setText('Измените')
+        else:
+            self.label.setText('Добавьте новое кофе')
 
 
 if __name__ == '__main__':
